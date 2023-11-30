a0c100ad6b6690bb8a403c3119189f8971082ef8
ngoan-23 <ngoquocan.work@gmail.com>
2023-11-30 14:38:33 +0700

    Build a GraphQL API Using Laravel

---
+++ database/factories/QuestFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use App\Models\Quest;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Quest>
+ */
+class QuestFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    protected $model = Quest::class;
+
+    public function definition(): array
+    {
+        $categoryIDs = Category::all()->pluck('id')->toArray();
+
+        return [
+            'title' => $this->faker->title(),
+            'description' => $this->faker->text(),
+            'reward' => $this->faker->numberBetween(1 , 100),
+            'category_id' => $this->faker->randomElement($categoryIDs)
+        ];
+    }
+}
